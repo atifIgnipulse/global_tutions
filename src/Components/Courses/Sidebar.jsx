2fcb0de2994ae8a6b1ea32bfb27385f5597ab55c
atifIgnipulse <atif.ignipulse@gmail.com>
2024-11-27 05:43:22 -0800

    atf

--- src/Components/Courses/Sidebar.jsx
+++ src/Components/Courses/Sidebar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import APIS from '../../../Config/Config'
+import axios from 'axios'
 
 const Sidebar = () => {
+
+  const [categories, setCategories] = useState([])
+
+
+  const getCategories = async()=>{
+
+    try {
+      const res = await axios.get(APIS.get_enabled_categories, {params: {order:1}})
+      if(res.statusText === "OK"){
+        setCategories(res.data);
+      }else{
+        // Render a dummy jsx to show message that says 'no categories available'
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(()=>{
+    getCategories();
+  },[])
+
+
   return (
-    <div className='border-2'>
-       <div class="lg:w-[20%] w-full md:px-0 px-6 bg-[#FFFFFF] p-2 ">
+    <div className='border-2 lg:w-[20%]'>
+       <div class="w-full md:px-0 px-6 bg-[#FFFFFF] p-2 ">
             <div class="flex flex-col px-4 gap-2 pt-8">
               <p
                 class="font-readex font-semibold lg:text-[38px] md:text-[31px] text-[25px] xl:text-[44px] 2xl:text-[51px]"
@@ -19,22 +45,14 @@ const Sidebar = () => {
                 class="text-[ 12px] md:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]"
               >
                 <div class="flex flex-col gap-1">
-                  <label class="flex items-center">
-                    <input type="checkbox" class="mr-2" />
-                    <a href="Beginner">web development</a>
-                  </label>
-                  <label class="flex items-center">
-                    <input type="checkbox" class="mr-2" />
-                    <a href="">programming</a>
-                  </label>
-                  <label class="flex items-center">
-                    <input type="checkbox" class="mr-2" />
-                    <a href="">database</a>
-                  </label>
-                  <label class="flex items-center">
-                    <input type="checkbox" class="mr-2" />
-                    <a href="">Microsoft office</a>
-                  </label>
+                  {
+                    categories.map((category, index)=> (
+                      <label key={index} class="flex items-center">
+                        <input type="checkbox" class="mr-2" />
+                        <a href="Beginner">{category.category_name}</a>
+                      </label>
+                    ))
+                  }
                 </div>
               </div>
             </div>
@@ -105,7 +123,7 @@ const Sidebar = () => {
              
               </div>
             </div>
-          </div>
+        </div>
     </div>
   )
 }
--- src/Components/Main/Cards.jsx
+++ src/Components/Main/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Cards = () => {
+const Cards = (props) => {
+  console.log("props:", props)
   return (
     <div>
          {/* <div class="md:px-14 lg:px- xl:px-24 2xl:px-32 w-full"> */}
@@ -13,7 +14,8 @@ const Cards = () => {
                 <div class="card-body">
                   <div class="card-body-text">
                     <p class="card-p">
-                      <span class="card-span">Duration :</span> 6hr
+                      {/* todo:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */}
+                      <span class="card-span">Duration :</span> 
                     </p>
                   </div>
                   <div>
@@ -23,7 +25,7 @@ const Cards = () => {
                   </div>
                 </div>
                 <div class="html-div">
-                  <p class="html-p">HTML for Beginners</p>
+                  <p class="html-p">{props}</p>
                   <p>
                     <i class="fa-solid fa-arrow-right-to-bracket"></i>
                   </p>
@@ -40,171 +42,7 @@ const Cards = () => {
                 </div>
               </div>
             </div>
-            {/* <!-- 2 --> */}
-            <div class="cards">
-              <div class="cards-color-header">
-                <div class="card-body">
-                  <div class="card-body-text">
-                    <p class="card-p">
-                      <span class="card-span">Duration :</span> 6hr
-                    </p>
-                  </div>
-                  <div>
-                    <i
-                      class="fa-solid fa-bookmark text-2xl align-middle"
-                    ></i>
-                  </div>
-                </div>
-                <div class="html-div">
-                  <p class="html-p">HTML for Beginners</p>
-                  <p>
-                    <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                  </p>
-                </div>
-              </div>
-              <div class="footer-div">
-                <div>
-                  <p class="web-development-bold">Web Development</p>
-                </div>
-                <div>
-                  <a href="other-pages/courses-detail.html">
-                    <button class="btnbutton">View</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <!-- 3 --> */}
-            <div class="cards">
-              <div class="cards-color-header">
-                <div class="card-body">
-                  <div class="card-body-text">
-                    <p class="card-p">
-                      <span class="card-span">Duration :</span> 6hr
-                    </p>
-                  </div>
-                  <div>
-                    <i
-                      class="fa-solid fa-bookmark text-2xl align-middle"
-                    ></i>
-                  </div>
-                </div>
-                <div class="html-div">
-                  <p class="html-p">HTML for Beginners</p>
-                  <p>
-                    <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                  </p>
-                </div>
-              </div>
-              <div class="footer-div">
-                <div>
-                  <p class="web-development-bold">Web Development</p>
-                </div>
-                <div>
-                  <a href="other-pages/courses-detail.html">
-                    <button class="btnbutton">View</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <!-- 4 --> */}
-            <div class="cards">
-              <div class="cards-color-header">
-                <div class="card-body">
-                  <div class="card-body-text">
-                    <p class="card-p">
-                      <span class="card-span">Duration :</span> 6hr
-                    </p>
-                  </div>
-                  <div>
-                    <i
-                      class="fa-solid fa-bookmark text-2xl align-middle"
-                    ></i>
-                  </div>
-                </div>
-                <div class="html-div">
-                  <p class="html-p">HTML for Beginners</p>
-                  <p>
-                    <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                  </p>
-                </div>
-              </div>
-              <div class="footer-div">
-                <div>
-                  <p class="web-development-bold">Web Development</p>
-                </div>
-                <div>
-                  <a href="other-pages/courses-detail.html">
-                    <button class="btnbutton">View</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <!-- 5 --> */}
-            <div class="cards">
-              <div class="cards-color-header">
-                <div class="card-body">
-                  <div class="card-body-text">
-                    <p class="card-p">
-                      <span class="card-span">Duration :</span> 6hr
-                    </p>
-                  </div>
-                  <div>
-                    <i
-                      class="fa-solid fa-bookmark text-2xl align-middle"
-                    ></i>
-                  </div>
-                </div>
-                <div class="html-div">
-                  <p class="html-p">HTML for Beginners</p>
-                  <p>
-                    <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                  </p>
-                </div>
-              </div>
-              <div class="footer-div">
-                <div>
-                  <p class="web-development-bold">Web Development</p>
-                </div>
-                <div>
-                  <a href="other-pages/courses-detail.html">
-                    <button class="btnbutton">View</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <!-- 6 --> */}
-            <div class="cards">
-              <div class="cards-color-header">
-                <div class="card-body">
-                  <div class="card-body-text">
-                    <p class="card-p">
-                      <span class="card-span">Duration :</span> 6hr
-                    </p>
-                  </div>
-                  <div>
-                    <i
-                      class="fa-solid fa-bookmark text-2xl align-middle"
-                    ></i>
-                  </div>
-                </div>
-                <div class="html-div">
-                  <p class="html-p">HTML for Beginners</p>
-                  <p>
-                    <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                  </p>
-                </div>
-              </div>
-              <div class="footer-div">
-                <div>
-                  <p class="web-development-bold">Web Development</p>
-                </div>
-                <div>
-                  <a href="other-pages/courses-detail.html">
-                    <button class="btnbutton">View</button>
-                  </a>
-                </div>
-              </div>
-            </div>
+            
           </div>
         </div>
       {/* </div> */}
